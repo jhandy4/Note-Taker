e84eafd0a6c39d8ced80a8ff7b8d86a2673ed6e6
jhandy4 <58635628+jhandy4@users.noreply.github.com>
2020-06-08 00:16:01 -0400

    working

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 var express = require("express");
 var path = require("path");
 var fs = require("fs");
-var PORT = process.env.PORT || 3500;
-let notes = require("./db/db.json");
+var PORT = process.env.PORT || 3000;
+var notes = require("./db/db.json");
 
 var app = express();
 // Sets up the express app to handle data parsing
@@ -15,6 +15,16 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.get("/notes", function(req, res) {
     res.sendFile(path.join(__dirname, "/public/notes.html"));
 });
+app.get("/api/notes", function(req,res) {
+    res.sendFile(path.join(__dirname, "/db/db.json"));
+});
+app.get("/api/notes/:id", function (err,res) {
+    notes = JSON.parse(fs.readFile(path.join(__dirname, "/db/db.json"), "utf8"), function (err, data) {
+        if (err) throw err;
+    });
+    res.json(notes[Number(req.params.id)]);
+    // res.json(data);
+});
 app.get("*", function(req, res) {
     res.send(path.join(__dirname, "/public/index.html"));
 });
@@ -23,42 +33,35 @@ app.get("*", function(req, res) {
 //     return res.sendFile(path.json(__dirname, "/db/db.json"));
 // });
 
-// let notes = [];
-
-app.get("/api/notes/", function (err,res) {
-    fs.readFile(path.join(__dirname, "/db/db.json"), "utf8", function (err) {
-        if (err) throw err;
-    });
-    res.send(JSON.parse(notes));
-    res.json(notes);
-});
-
-
 //   * POST `/api/notes` - Should receive a new note to save on the request body, add it to the `db.json` file, and then return the new note to the client.
 //   * DELETE `/api/notes/:id` - Should receive a query parameter containing the id of a note to delete. This means you'll need to find a way to give each note a unique `id` when it's saved. In order to delete a note, you'll need to read all notes from the `db.json` file, remove the note with the given `id` property, and then rewrite the notes to the `db.json` file.
 app.post("/api/notes", function(req,res) {
-    notes = fs.readFileSync(path.join(__dirname, "/db/db.json"), "utf8");
-    notes = JSON.parse(notes);
-    req.body.id = notes.length +1;
-    notes.push(req.body);
-    notes = JSON.stringify(notes);
-    fs.writeFile(path.join(__dirname, "/db/db.json"), notes, "utf8", function(err) {
+    notes = JSON.parse(fs.readFileSync(path.join(__dirname, "/db/db.json"), "utf8"));
+    // data = JSON.parse(notes);
+    newNote = req.body;
+    // req.body.id = notes.length +1;
+    arrayId = (notes.length).toString();
+    newNote.id = arrayId;
+    notes.push(newNote);
+    fs.writeFileSync(path.join(__dirname, "/db/db.json"), JSON.stringify(notes), "utf8", function(err) {
         if (err) throw err;
     });
-    res.json(JSON.parse(notes));
+    res.json(notes);
 });
 
 app.delete("/api/notes/:id", function(req,res) {
-    notes = fs.readFileSync(path.join(__dirname, "/db/db.json"), "utf8");
-    notes = JSON.parse(notes);
-    notes = notes.filter(function(note) {
-      return note.id != req.params.id;
+    notes = JSON.parse(fs.readFileSync(path.join(__dirname, "/db/db.json"), "utf8"));
+    // notes = JSON.parse(notes);
+    noteId = req.params.id;
+    notes = notes.filter(selectNote => {
+        return selectNote.id != noteId;
     });
-    notes = JSON.stringify(notes);
-    fs.writeFile(path.join(__dirname, "/db/db.json"), notes, "utf8", function(err) {
-    if (err) throw err;
+    // notes = JSON.stringify(notes);
+    fs.writeFileSync(path.join(__dirname, "/db/db.json"), JSON.stringify(notes), "utf8", function(err) {
+        if (err) throw err;
     });
-    res.send(JSON.parse(notes));
+    res.json(notes);
+
 });
 
 
